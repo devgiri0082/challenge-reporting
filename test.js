9787de89a37ba4f0e338902ffd9def04ff48adec
Dev Giri <devgiri0082@gmail.com>
2023-07-14 19:35:20 +0545

    feat: added test for calculateGradeStats

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const tape = require('tape')
 const jsonist = require('jsonist')
+const { calculateGradeStats } = require('./api')
 
 const port = (process.env.PORT = process.env.PORT || require('get-port-sync')())
 const endpoint = `http://localhost:${port}`
@@ -88,12 +89,105 @@ tape('GET /student/:id/grades endpoint', function (t) {
 })
 
 tape('GET /course/all/grades endpoint', function (t) {
+  const grades = [
+    {
+      id: 1,
+      course: 'Calculus',
+      grade: 50
+    },
+    {
+      id: 1,
+      course: 'Microeconomics',
+      grade: 43
+    },
+    {
+      id: 1,
+      course: 'Statistics',
+      grade: 50
+    },
+    {
+      id: 1,
+      course: 'Astronomy',
+      grade: 63
+    },
+    {
+      id: 2,
+      course: 'Calculus',
+      grade: 9
+    },
+    {
+      id: 2,
+      course: 'Microeconomics',
+      grade: 11
+    },
+    {
+      id: 3,
+      course: 'Microeconomics',
+      grade: 38
+    },
+    {
+      id: 4,
+      course: 'Philosophy',
+      grade: 79
+    },
+    {
+      id: 4,
+      course: 'Calculus',
+      grade: 1
+    },
+    {
+      id: 4,
+      course: 'Microeconomics',
+      grade: 10
+    },
+    {
+      id: 5,
+      course: 'Microeconomics',
+      grade: 69
+    },
+    {
+      id: 5,
+      course: 'Philosophy',
+      grade: 54
+    },
+    {
+      id: 5,
+      course: 'Statistics',
+      grade: 22
+    }
+  ]
+
+  const expectedStats = {
+    highestGrade: [
+      { id: 1, course: 'Calculus', grade: 50 },
+      { id: 5, course: 'Microeconomics', grade: 69 },
+      { id: 1, course: 'Statistics', grade: 50 },
+      { id: 1, course: 'Astronomy', grade: 63 },
+      { id: 4, course: 'Philosophy', grade: 79 }
+    ],
+    lowestGrade: [
+      { id: 4, course: 'Calculus', grade: 1 },
+      { id: 4, course: 'Microeconomics', grade: 10 },
+      { id: 5, course: 'Statistics', grade: 22 },
+      { id: 1, course: 'Astronomy', grade: 63 },
+      { id: 5, course: 'Philosophy', grade: 54 }
+    ],
+    averageGrade: [
+      { id: 1, course: 'Calculus', average: 20 },
+      { id: 1, course: 'Microeconomics', average: 34.2 },
+      { id: 1, course: 'Statistics', average: 36 },
+      { id: 1, course: 'Astronomy', average: 63 },
+      { id: 4, course: 'Philosophy', average: 66.5 }
+    ]
+  }
+
   t.test('should return object highestGrade, lowestGrade and averageGrade', async (t) => {
     const statusCode = 200
     const url = `${endpoint}/course/all/grades`
 
     try {
       const { data, response } = await jsonist.get(url)
+      t.deepEqual(calculateGradeStats(grades), expectedStats)
       t.equal(statusCode, response.statusCode)
       t.assert(!!data.highestGrade)
       t.assert(!!data.lowestGrade)
